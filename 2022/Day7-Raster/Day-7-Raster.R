568482f8434175c7889bdb205ac4ad30a66c068a
BB1464 <oluwafemioyedele908@gmail.com>
2022-11-07 12:46:31 +0100

    Add Readme file

--- 2022/Day7-Raster/Day-7-Raster.R
+++ 2022/Day7-Raster/Day-7-Raster.R
@@ -3,35 +3,51 @@
 # Load the Required Library ---------------------------------
 
 library(tidyverse)
-library(camcorder)
 library(afrilearndata)
+library(raster)
+library(sp)
 library(scico)
+library(showtext)
 
-gg_record(dir = "2021/temp", device = "png", width = 10, height = 10, units = "in", dpi = 320)
+
+
 
 afripop_df <- afripop2020 %>% 
   as.data.frame(xy = TRUE) %>% 
   rename(pop = 3) %>% 
   filter(!is.na(pop))
 
-f1 = "Porpora"
-f2 = "Publico Headline"
+# Font
+
+font_add_google("Fira Sans","Fira Sans")
+
+f1 = "Fira Sans"
+
+showtext_auto(enable = TRUE)
+showtext_opts(dpi=320)
+
 
 ggplot(afripop_df) +
   geom_tile(aes(x, y, fill = pop)) +
-  annotate("text", -6, -5, label = "Africa", family = f2, size = 20, fontface = "bold", color = "grey97") +
+  annotate("text", -6, -5, label = "Africa", family = f1, size = 20, fontface = "bold", color = "grey97") +
   scale_fill_scico(direction = -1, trans = "pseudo_log", palette = "lajolla", breaks = c(0, 100, 1000, 10000, 20000)) +
   guides(fill = guide_colorbar(title = "Population density\n(people/km²)", label.position = "left", title.hjust = 0.5)) +
-  labs(caption = "Data: afrilearndata · Graphic: Georgios Karamanis") +
+  labs(caption = "Data: afrilearndata · Graphic: Oluwafemi Oyedele") +
   coord_fixed() +
   theme_void(base_family = f1, base_size = 14) +
   theme(
     legend.position = c(0.22, 0.23),
-    legend.key.width = unit(0.5, "line"),
+    legend.key.width = unit(0.6, "line"),
     legend.key.height = unit(2, "line"),
     legend.title = element_text(margin = margin(0, 0, 10, -20), color = "grey85", lineheight = 1.1),
     legend.text = element_text(color = "grey85"),
-    plot.background = element_rect(fill = "#58507E", color = NA),
-    plot.caption = element_text(hjust = 1, color = "grey80"),
-    plot.margin = margin(10, 10, 10, 10)
+    plot.background = element_rect(fill = "#58507E", color = "#58507E"),
+    plot.caption = element_text(hjust = 1, color = "white",size = 20,face = 'bold'),
+    plot.margin = margin(10, 10, 10, 10),panel.background = element_rect(fill = '#58507E',colour = NA)
   )
+
+
+
+# Save the Plot
+
+ggsave('Day7.png',path = here::here('2022/Day7-Raster/'),width = 12,height = 12,dpi = 230)
---
+++ 2022/Day7-Raster/README.md
@@ -0,0 +1,22 @@
+# 30DayMapChallenge 
+
+## Day7: Raster
+
+<h1 align="center"> Day 7: African Population Density !!! </h1>
+  
+  <p align="center">
+    <img src="https://github.com/BB1464/30DayMapChallenge/blob/master/2022/Day7-Raster/Day7.png" width="60%">
+      </p>
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+
+
+
